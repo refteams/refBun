2fb0eb526cfb23167e2744d71e20a1d6ec46625b
PresentKim <qjrmckd@gmail.com>
2018-02-12 03:34:31 +0900

    Utils : Add in_arrayi() method

--- src/presentkim/makepluginplus/util/Utils.php
+++ src/presentkim/makepluginplus/util/Utils.php
@@ -5,6 +5,21 @@ namespace presentkim\makepluginplus\util;
 class Utils{
 
     /**
+     * @param string $str
+     * @param array  $strs
+     *
+     * @return bool
+     */
+    public static function in_arrayi(string $str, array $strs) : bool{
+        foreach ($strs as $key => $value) {
+            if (strcasecmp($str, $value) === 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
      * @param Object[] $list
      *
      * @return string[]
