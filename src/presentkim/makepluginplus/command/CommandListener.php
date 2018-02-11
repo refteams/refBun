1c2e0c796e68f7ee0b0e408a3e53cbfa790a5636
PresentKim <qjrmckd@gmail.com>
2018-02-12 02:38:54 +0900

    CommandListener : Use buildPhar() method

--- src/presentkim/makepluginplus/command/CommandListener.php
+++ src/presentkim/makepluginplus/command/CommandListener.php
@@ -60,39 +60,11 @@ class CommandListener implements CommandExecutor{
             if (!file_exists($dataFolder = $this->owner->getDataFolder())) {
                 mkdir($dataFolder, 0777, true);
             }
-            $setting = $this->owner->getConfig()->getAll();
             foreach ($plugins as $pluginName => $plugin) {
                 $description = $plugin->getDescription();
                 $pharPath = $dataFolder . Translation::translate('phar-name', $pluginName, $pluginVersion = $description->getVersion());
-                if (file_exists($pharPath)) {
-                    \Phar::unlinkArchive($pharPath);
-                }
-                $phar = new \Phar($pharPath);
-                $phar->setSignatureAlgorithm(\Phar::SHA1);
-                if (!$setting['skip-metadata']) {
-                    $phar->setMetadata([
-                      'name'         => $pluginName,
-                      'version'      => $pluginVersion,
-                      'main'         => $description->getMain(),
-                      'api'          => $description->getCompatibleApis(),
-                      'depend'       => $description->getDepend(),
-                      'description'  => $description->getDescription(),
-                      'authors'      => $description->getAuthors(),
-                      'website'      => $description->getWebsite(),
-                      'creationDate' => time(),
-                    ]);
-                }
-                if ($setting['skip-stub']) {
-                    $phar->setStub('<?php echo "PocketMine-MP plugin ' . "{$pluginName}_v{$pluginVersion}\nThis file has been generated using MakePluginPlus at " . date("r") . '\n----------------\n";if(extension_loaded("phar")){$phar = new \Phar(__FILE__);foreach($phar->getMetadata() as $key => $value){echo ucfirst($key).": ".(is_array($value) ? implode(", ", $value):$value)."\n";}} __HALT_COMPILER();');
-                } else {
-                    $phar->setStub('<?php __HALT_COMPILER();');
-                }
-                $phar->startBuffering();
-                $phar->buildFromDirectory(rtrim(str_replace("\\", '/', $fileProperty->getValue($plugin)), '/') . '/');
-                if ($setting['compress'] && \Phar::canCompress(\Phar::GZ)) {
-                    $phar->compressFiles(\Phar::GZ);
-                }
-                $phar->stopBuffering();
+                $filePath = rtrim(str_replace("\\", '/', $fileProperty->getValue($plugin)), '/') . '/';
+                $this->owner->buildPhar($plugin, $filePath, $pharPath);
                 $sender->sendMessage(Translation::translate('command-makepluginplus@build', $pluginName, $pluginVersion, $pharPath));
             }
             $sender->sendMessage(Plugin::$prefix . Translation::translate('command-makepluginplus@built', count($plugins)));
