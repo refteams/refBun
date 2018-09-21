c81167500035ca585aa0654c88f8969660cabacb
PresentKim <qjrmckd@gmail.com>
2018-09-21 22:23:23 +0900

    Add HEAD PHP DOC

--- src/kim/present/makepluginplus/util/Utils.php
+++ src/kim/present/makepluginplus/util/Utils.php
@@ -1,5 +1,25 @@
 <?php
 
+/*
+ *
+ *  ____                           _   _  ___
+ * |  _ \ _ __ ___  ___  ___ _ __ | |_| |/ (_)_ __ ___
+ * | |_) | '__/ _ \/ __|/ _ \ '_ \| __| ' /| | '_ ` _ \
+ * |  __/| | |  __/\__ \  __/ | | | |_| . \| | | | | | |
+ * |_|   |_|  \___||___/\___|_| |_|\__|_|\_\_|_| |_| |_|
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the MIT License. see <https://opensource.org/licenses/MIT>.
+ *
+ * @author  PresentKim (debe3721@gmail.com)
+ * @link    https://github.com/PresentKim
+ * @license https://opensource.org/licenses/MIT MIT License
+ *
+ *   (\ /)
+ *  ( . .) ♥
+ *  c(")(")
+ */
+
 declare(strict_types=1);
 
 namespace kim\present\makepluginplus\util;
