99ac246f0024fdaf17906d02546225fa6850637b
PresentKim <debe3721@gmail.com>
2020-08-05 01:33:28 +0900

    Remove Utils::in_arrayi()

--- src/blugin/makepluginplus/util/Utils.php
+++ src/blugin/makepluginplus/util/Utils.php
@@ -250,21 +250,6 @@ class Utils{
     }
 
     /**
-     * @param string $str
-     * @param array  $strs
-     *
-     * @return bool
-     */
-    public static function in_arrayi(string $str, array $strs) : bool{
-        foreach($strs as $key => $value){
-            if(strcasecmp($str, $value) === 0){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
      * @param string $originalCode
      *
      * @return string
